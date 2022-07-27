8582582b81d371b2988c29c68e09b1f94f6d9755
Antoine Latrille <antoine.latrille@gmail.com>
2022-07-27 13:02:07 +0200

    Squelette de routeur + vues simples sans queryset

--- api/users/views.py
+++ api/users/views.py
@@ -1,9 +1,12 @@
 # from django.shortcuts import render
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, ViewSet
 
 # from users.models import User
 # from users.serializers import UserSerializer, SignUpSerializer
@@ -13,18 +16,39 @@ from rest_framework.viewsets import ModelViewSet
 from users.serializers import UserSerializer, SignUpSerializer
 
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(APIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
-    def get_queryset(self):
-        return get_user_model().objects.all()
+    # @route('/me')
+    def get(self, request):
+        return request.user
 
 
-class SignUpViewSet(CreateAPIView):
+class SignupView(APIView):
     serializer_class = SignUpSerializer
     permission_classes = (AllowAny,)
 
-    def get_queryset(self):
-        return get_user_model().objects.all()
-
+    def post(self):
+        return Response(data={'result': 'ok'}, content_type='application/json')
+#
+#
+# class AccountView(APIView):
+#     serializer_class = SignUpSerializer
+#     permission_classes = (AllowAny,)
+#
+#     def post(self):
+#         # signu
+#         print('get in queryset')
+#         return get_user_model().objects.all()
+#
+#     @action(methods=['POST'], detail=False, permission_classes=[AllowAny], url_name='login')
+#     def login(self, truc):
+#         print(f'coucou {truc}')
+#         return Response(data={'result': 'ok'}, content_type='application/json')
+#
+#
+#
+# # POST /signup
+# # POST /login
+# # /users

a45a56808ecd27258a574e97caa8c16b691421da
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-02 18:35:45 +0200

    #3 #7 Updated settings
    Updated settings to take apps into account:
    - Considered projects and issues apps in INSTALLED_APPS

--- api/api/settings.py
+++ api/api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'users',
+    'projects',
+    'issues',
 ]
 
 REST_FRAMEWORK = {
@@ -51,6 +53,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+# TODO Delete SIMPLE_JWT to avoid excessive timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 MIDDLEWARE = [

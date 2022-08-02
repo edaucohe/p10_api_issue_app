46f48b7d5e16560ca5b777d5fedb4b3e0087d231
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-02 18:32:27 +0200

    #2 Updated sign up feature
    Updated sign up feature:
    - Implemented SignUpViewSet in users/views.py

--- api/users/views.py
+++ api/users/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -8,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ViewSet
 
-# from users.models import User
+from users.models import User
 # from users.serializers import UserSerializer, SignUpSerializer
 
 
@@ -16,21 +17,31 @@ from rest_framework.viewsets import ModelViewSet, ViewSet
 from users.serializers import UserSerializer, SignUpSerializer
 
 
-class UserViewSet(APIView):
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
+    http_method_names = ['get', 'post']
 
     # @route('/me')
-    def get(self, request):
-        return request.user
+    @action(detail=True, methods=['get'])
+    def get_users(self):
+        # users = User.objects.all()
+        queryset = User.objects.filter(active=True)
+        username = self.request.GET.get('username')
+        if username is not None:
+            queryset = queryset.filter(username=username)
+        # user = User.objects.filter(Q(user=user))
+        # serializer = self.serializer_class(user, many=True)
+        return queryset
 
 
-class SignupView(APIView):
+class SignUpViewSet(CreateAPIView):
     serializer_class = SignUpSerializer
     permission_classes = (AllowAny,)
 
-    def post(self):
-        return Response(data={'result': 'ok'}, content_type='application/json')
+    def get_queryset(self):
+        return get_user_model().objects.all()
 #
 #
 # class AccountView(APIView):

9e49f7c98ff33c07288d52e246c09ef98c960de8
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-02 18:04:38 +0200

    #6 Created contributor model
    Created contributor model:
    - Created contributor model in users/models.py
    - Updated migrations for contributor model

--- api/users/models.py
+++ api/users/models.py
@@ -1,6 +1,20 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from projects.models import Project
 
 
 class User(AbstractUser):
     pass
+
+
+class Contributor(models.Model):
+    TYPE_OF_ROLE = [
+        ("AUTHOR", "Author"),
+        ("CONTRIBUTOR", "Contributor")
+    ]
+
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=TYPE_OF_ROLE, verbose_name='Type of role')

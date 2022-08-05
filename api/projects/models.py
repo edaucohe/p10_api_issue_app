3b38822d198b504ab702754d04c2530c6a1a88ae
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-05 17:33:08 +0200

    #3 Updated Project model
    Updated Project model in projects/models.py to use Enum for choices parameter

--- api/projects/models.py
+++ api/projects/models.py
@@ -4,15 +4,14 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    TYPE_OF_PROJECT = [
-        ("BACK-END", "Back-End"),
-        ("FRONT-END", "Front-End"),
-        ("IOS", "iOS"),
-        ("ANDROID", "Android")
-    ]
+    class ProjectType(models.TextChoices):
+        BACK_END = ("BACK-END", "Back-End")
+        FRONT_END = ("FRONT-END", "Front-End")
+        IOS = ("IOS", "iOS")
+        ANDROID = ("ANDROID", "Android")
 
     title = models.CharField(max_length=128)
     description = models.TextField(max_length=2048)
     created_time = models.DateTimeField(auto_now_add=True)
-    type = models.CharField(max_length=15, choices=TYPE_OF_PROJECT, verbose_name="type of project")
+    type = models.CharField(max_length=15, choices=ProjectType.choices, verbose_name="type of project")
     author_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)

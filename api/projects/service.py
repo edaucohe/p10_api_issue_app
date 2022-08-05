1d8c8d59b96a80b5554676194b8d76ec98c9f77f
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-05 17:42:38 +0200

    #5 Updated projects endpoints
    Updated 'projects' endpoints to take user authorization into account:
    - Created service.py file to evaluate access/edition authorization of users
    - Implemented list method (ProjectViewSet of projects/views.py) to list projects collaborations of user
    - Updated retrieve method to evaluate role of user
    - Updated update method to evaluate role of user
    - Updated destroy method to evaluate role of user

--- api/projects/service.py
+++ api/projects/service.py
@@ -1,11 +1,18 @@
 from typing import Optional
 
-from projects.models import Project
 from users.models import Contributor, User
+from projects.models import Project
 
 
 def can_user_access_project(project: Project, user: User, role: Optional[Contributor.Role] = None) -> bool:
     return Contributor.objects.filter(project=project, user=user, role=role).exists()
+
+
+def can_user_edit_project(project: Project, user: User, role: Optional[Contributor.Role] = None) -> bool:
+    if role == 'AUTHOR':
+        return Contributor.objects.filter(project=project, user=user, role=role).exists()
+    else:
+        return False
 
 # issues
 # comments
--- api/projects/views.py
+++ api/projects/views.py
@@ -1,17 +1,17 @@
 # from django.shortcuts import render
-from typing import Optional
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from rest_framework import status
-# from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import TokenAuthentication
 
-from projects.models import Project
-
-from projects.serializers import ProjectSerializer
-from users.models import Contributor
 from projects import service
+from projects.serializers import ProjectSerializer
+from projects.models import Project
+from users.models import Contributor
 
 
 class ProjectViewSet(ModelViewSet):
@@ -21,9 +21,13 @@ class ProjectViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'delete']
 
     def get_queryset(self):
-        # Afficher la liste des projets rattachés à l'utilisateur
-        queryset = Project.objects.filter(author_user=self.request.user)
-        return queryset
+        return Project.objects.all()
+
+    def list(self, request):
+        user = self.request.user
+        projects = [contributor.project for contributor in Contributor.objects.filter(user=user)]
+        projects = sorted(projects, key=lambda order_by: order_by.id)
+        return Response(self.serializer_class(projects, many=True).data, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
         user = request.user
@@ -40,43 +44,63 @@ class ProjectViewSet(ModelViewSet):
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self, request, pk=None):
-        user = request.user
-        project = self.get_object()
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        try:
+            user = request.user
+            project = self.get_object()
 
-        is_user_authorized = service.can_user_access_project(project, user)
-        if is_user_authorized:
-            response = Response(self.serializer_class(project).data, status=status.HTTP_200_OK)
-        else:
-            response = Response({'message': 'You are not a contributor'}, status=status.HTTP_403_FORBIDDEN)
-        return response
+            user_role = Contributor.objects.filter(user=user, project=project).get().role
+            is_user_authorized = service.can_user_access_project(project, user, role=user_role)
+            if is_user_authorized:
+                return Response(self.serializer_class(project).data, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'You are not a contributor'}, status=status.HTTP_403_FORBIDDEN)
+
+        except ObjectDoesNotExist:
+            return Response({'message': 'You have not access to the project'}, status=status.HTTP_403_FORBIDDEN)
 
     def update(self, request, pk=None, *args, **kwargs):
-        user = request.user
-        project: Project = self.get_object()
-        is_user_authorized = can_user_access_project(project, user, role=Contributor.Role.Author)
+        try:
+            user = request.user
+            project: Project = self.get_object()
 
-        if not is_user_authorized:
-            return Response({'message': 'You must be the author to update'}, status=status.HTTP_403_FORBIDDEN)
+            user_role = Contributor.objects.filter(user=user, project=project).get().role
+            is_user_authorized = service.can_user_edit_project(project, user, role=user_role)
+            if not is_user_authorized:
+                return Response({'message': 'You must be the author to update.'}, status=status.HTTP_403_FORBIDDEN)
 
-        data = {
-            "title": request.POST.get('title', None),
-            "description": request.POST.get('description', None),
-            "type": request.POST.get('type', None),
-            }
-        serializer = self.serializer_class(
-            instance=project,
-            data=data,
-            context={'author': user},
-            partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            data = {
+                "title": request.POST.get('title', None),
+                "description": request.POST.get('description', None),
+                "type": request.POST.get('type', None),
+                }
+            serializer = self.serializer_class(
+                instance=project,
+                data=data,
+                context={'author': user},
+                partial=True
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except ObjectDoesNotExist:
+            return Response({'message': 'You have not access to the project'}, status=status.HTTP_403_FORBIDDEN)
 
     def destroy(self, request, pk=None, *args, **kwargs):
-        instance = self.get_object()
-        # you custom logic #
-        return super(ProjectViewSet, self).destroy(request, pk, *args, **kwargs)
+        try:
+            user = request.user
+            project: Project = self.get_object()
+
+            user_role = Contributor.objects.filter(user=user, project=project).get().role
+            is_user_authorized = service.can_user_edit_project(project, user, role=user_role)
+
+            if not is_user_authorized:
+                return Response({'message': 'You must be the author to delete.'}, status=status.HTTP_403_FORBIDDEN)
+
+            return super(ProjectViewSet, self).destroy(request, pk, *args, **kwargs)
+
+        except ObjectDoesNotExist:
+            return Response({'message': 'You have not access to the project'}, status=status.HTTP_403_FORBIDDEN)

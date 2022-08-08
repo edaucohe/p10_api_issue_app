41682a04475530f94cd423481293f18bbadbefa1
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-09 00:05:17 +0200

    #3 Updated ProjectSerializer class
    Added CharField in ProjectSerializer class of projects/serializer.py to display right info of fields

--- api/projects/serializers.py
+++ api/projects/serializers.py
@@ -1,12 +1,23 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from projects.models import Project
 
 
 class ProjectSerializer(ModelSerializer):
+    type = CharField(source='get_type_display', read_only=True)
+    author_user = CharField(source="author_user.username", read_only=True)
+
     class Meta:
         model = Project
-        fields = '__all__'
+        # fields = '__all__'
+        fields = [
+            'id',
+            'author_user',
+            'title',
+            'description',
+            'created_time',
+            'type'
+        ]
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)

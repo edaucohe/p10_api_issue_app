b0508bce0364277ecfd660531b73a203ffcf521c
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-08 16:29:27 +0200

    #7 Updated IssueViewSet
    Updated IssueViewSet to display/create option only for contributors:
    - Added list method to display issues of contributors
    - Updated create method to allow contributors can create an issue

--- api/issues/views.py
+++ api/issues/views.py
@@ -1,15 +1,16 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from issues.serializers import IssueSerializer
-
 from issues.models import Issue
-from users.models import User
+from users.models import User, Contributor
+
+from projects import service
+from projects.models import Project
 
 
 class IssueViewSet(ModelViewSet):
@@ -19,20 +20,35 @@ class IssueViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'delete']
 
     def get_queryset(self):
-        # Display issues list of a project
-        queryset = Issue.objects.filter(author_user=self.request.user)
+        queryset = Issue.objects.all()
         return queryset
 
-    def create(self, request, *args, **kwargs):
+    def list(self, request, project_pk=None, *args, **kwargs):
+        try:
+            user = self.request.user
+            project = Project.objects.filter(pk=project_pk).get()
+
+            user_role = Contributor.objects.filter(user=user, project=project_pk).get().role
+            is_user_authorized = service.can_user_access_project(project, user, role=user_role)
+            if not is_user_authorized:
+                return Response({'message': 'You are not a contributor'}, status=status.HTTP_403_FORBIDDEN)
+
+            issues = list(Issue.objects.filter(project=project_pk).order_by('id'))
+            return Response(self.serializer_class(issues, many=True).data, status=status.HTTP_200_OK)
+
+        except ObjectDoesNotExist:
+            return Response({'message': 'You have not access to the project or projects does not exist'},
+                            status=status.HTTP_403_FORBIDDEN)
+
+    def create(self, request, project_pk=None, *args, **kwargs):
         user = request.user
+        assignee_user_pk = user.pk
 
         assignee_user_name = request.POST.get('assignee_user', None)
-        assignee_user_pk = user.pk
-        # TODO To replace 'User.objects.all()' for contributors of the same project
-        users = User.objects.all()
-        for assignee_user in users:
-            if assignee_user_name == assignee_user.username:
-                assignee_user_pk = assignee_user.pk
+        contributors = list(Contributor.objects.filter(project=project_pk))
+        for assignee_user in contributors:
+            if assignee_user_name == assignee_user.user.username:
+                assignee_user_pk = assignee_user.user_id
 
         data = {
             "title": request.POST.get('title', None),

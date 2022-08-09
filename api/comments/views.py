94e6ce5f0c76cf392d826171f84502a6bf0cf87c
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-08-09 20:18:08 +0200

    #10 Display a list of comments of an issue
    Implemented list method to display the comment list of an issue with verification:
    - Updated CommentViewSet of comments/views.py

--- api/comments/views.py
+++ api/comments/views.py
@@ -1,9 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from comments.serializers import CommentSerializer
 from comments.models import Comment
+
+from issues.models import Issue
+from projects.models import Project
+from users.models import Contributor
+
+from projects import service
 
 
 class CommentViewSet(ModelViewSet):
@@ -15,3 +24,26 @@ class CommentViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = Comment.objects.all()
         return queryset
+
+    def list(self, request, project_pk=None, issues_pk=None, *args, **kwargs):
+        try:
+            user = self.request.user
+            project = Project.objects.filter(pk=project_pk).get()
+            issue = Issue.objects.filter(pk=issues_pk).get()
+            issue_project = issue.project
+
+            if not issue_project == project:
+                return Response({'message': 'Issue does not correspond to the project'},
+                                status=status.HTTP_406_NOT_ACCEPTABLE)
+
+            user_role = Contributor.objects.filter(user=user, project=project).get().role
+            is_user_authorized = service.can_user_access_project(project, user, role=user_role)
+            if not is_user_authorized:
+                return Response({'message': 'You are not a contributor'}, status=status.HTTP_403_FORBIDDEN)
+
+            comments = list(Comment.objects.filter(issue=issues_pk).order_by('id'))
+            return Response(self.serializer_class(comments, many=True).data, status=status.HTTP_200_OK)
+
+        except ObjectDoesNotExist:
+            return Response({'message': 'You have not access to the project or issue does not exist'},
+                            status=status.HTTP_403_FORBIDDEN)
